67ed8cc948446610d1096474df703e4987e8b154
Tomas Morstein <tmr@idea.cz>
2014-02-24 14:15:09 +0100

    include ms_transform to fix fun2ms use
    fun2ms requires ms_transform.hrl to be included!

--- src/check_server.erl
+++ src/check_server.erl
@@ -12,6 +12,8 @@
 -export([init/1, handle_call/3, handle_cast/2,
     handle_info/2, terminate/2, code_change/3]).
 
+-include_lib("stdlib/include/ms_transform.hrl").
+
 -record(checkstate, {weight, checkets}).
 
 %%%===================================================================
